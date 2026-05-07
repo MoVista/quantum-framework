2c28499d7620c1d72ace2c8e1fb6c4064f3601f3
Patrick Wolf <patrick.wolf@movista.com>
2026-05-07 12:35:28 -0400

    Revert "Add missing realm display name (#13)"
    This reverts commit 979187917df93d7502ffd7c3f8d7c3f9ca3e4985.

--- quantum-framework/src/main/java/com/e2eq/framework/rest/resources/UserProfileResource.java
+++ quantum-framework/src/main/java/com/e2eq/framework/rest/resources/UserProfileResource.java
@@ -2,9 +2,7 @@ package com.e2eq.framework.rest.resources;
 
 import com.e2eq.framework.model.persistent.base.ActiveStatus;
 import com.e2eq.framework.model.persistent.morphia.CredentialRepo;
-import com.e2eq.framework.model.persistent.morphia.RealmRepo;
 import com.e2eq.framework.model.security.CredentialUserIdPassword;
-import com.e2eq.framework.model.security.Realm;
 import com.e2eq.framework.model.auth.AuthProviderFactory;
 import com.e2eq.framework.rest.filters.PermissionCheck;
 import com.e2eq.framework.rest.models.RestError;
@@ -34,9 +32,6 @@ public class UserProfileResource extends BaseResource<UserProfile, UserProfileRe
 
    @Inject
    protected CredentialRepo credentialRepo;
-
-   @Inject
-   protected RealmRepo realmRepo;
 
    protected UserProfileResource (UserProfileRepo repo ) {
       super(repo);
@@ -130,8 +125,6 @@ public class UserProfileResource extends BaseResource<UserProfile, UserProfileRe
             CredentialUserIdPassword cred = ocred.get();
             CredentialUserIdPassword.RealmEntry realmEntry = new CredentialUserIdPassword.RealmEntry();
             realmEntry.setRealmRefName(realmRefName);
-            Optional<Realm> orealm = realmRepo.findByRefName(realmRefName, true);
-            realmEntry.setRealmDisplayName(orealm.map(Realm::getDisplayName).orElse(realmRefName));
             List<CredentialUserIdPassword.RealmEntry> realms = new ArrayList<>();
             realms.add(realmEntry);
             cred.setAuthorizedRealms(realms);
